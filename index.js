5b6f47d1efac390ae6373f1011bbfc90f3d1f287
temmie8731979878 <kapoo950807@gmail.com>
2022-04-10 20:48:35 +0800

    4/10 update
    增加酷酷的多選勾勾
    配色一如往常的糟糕

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// 地標座標
+// 地圖
 var list = [
   [25.061339, 121.518562, "pointer/red.png", "<h2>成淵高中</h2>\n測試用"],
   [25.063732, 121.513858, "pointer/red.png", "<h2>民權國中</h2>\n測試用"],
@@ -9,11 +9,10 @@ var list = [
 
   [25.062850, 121.518676, "pointer/yellow.png", "<h2>公車站_1</h2>\n測試用"],
   [25.062926, 121.516451, "pointer/yellow.png", "<h2>公車站_2</h2>\n測試用"],
-]
-
+];
 function initMap(){
   var location = {lat: 25.061339, lng: 121.518562}; // lat: 經度, lng: 緯度
-  function addmarker(lat, lng, icon, content){ // 增加地標
+  function addmarker(i, lat, lng, icon, content){ // 增加地標
     let p = {lat: lat, lng: lng}
     let marker = new google.maps.Marker({
       position: p,
@@ -25,9 +24,17 @@ function initMap(){
       content: content,
     });
   
-    marker.addListener("click", function(){
-      infoWindow.open(map, marker)
-    })
+    // info
+    marker.addListener("dblclick", function(){
+      infoWindow.open(map, marker);
+    });
+
+    marker.addListener("mouseout", function(){ 
+      marker.setAnimation(null);
+    });
+    marker.addListener("mouseover", function(){
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    });
   };
 
   var map = new google.maps.Map(document.getElementById("map"), { // 地圖初始化
@@ -40,7 +47,29 @@ function initMap(){
 
   // 輸出畫面
   for (var i=0 ; i<list.length ; i++){
-    addmarker(list[i][0], list[i][1], list[i][2], list[i][3])
+    addmarker(i, list[i][0], list[i][1], list[i][2], list[i][3])
+  }
+};
+
+// 選項
+var color = ["#FF776B", "#FFED5C", "#BCE3FF", "#BFC0C0"];
+var now_color = [1, 1, 1];
+function change_color(x){
+  now_color[x] = !now_color[x];
+  console.log(x)
+  console.log(now_color)
+
+  if (now_color[x] == 0){
+    if (x == 0){
+      document.getElementById(`c${x+1}`).style = `background-color: #BFC0C0; margin-left: 0px;`;
+    }else{
+      document.getElementById(`c${x+1}`).style = `background-color: #BFC0C0;`;
+    }
+  }else{
+    if (x == 0){
+      document.getElementById(`c${x+1}`).style = `background-color: ${color[x]}; margin-left: 0px;`;
+    }else{
+      document.getElementById(`c${x+1}`).style = `background-color: ${color[x]};`;
+    }
   }
 }
-
