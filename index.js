c9af3a0b4edf217e1259161c4080bf22c80be75b
temmie8731979878 <kapoo950807@gmail.com>
2022-06-10 11:29:05 +0800

    6/10 update

--- index.js
+++ index.js
@@ -40,8 +40,9 @@ var list = [
   [25.063309, 121.520942, "pointer/yellow.png", "民權台菜自助餐", "10491台北市大同區民權西路47號", "0225997080"],
 
   // 飲料
-  [25.063278, 121.513590, "pointer/blue.png", "大橋頭站", "測試用", "0000000000"],
-  [25.062917, 121.520162, "pointer/blue.png", "民權西路站", "測試用", "0000000000"]
+  [25.057686, 121.517616, "pointer/blue.png", "CoCo都可(台北民生店)", "103台北市大同區民生西路159號", " 0225531569"],
+  [25.066121, 121.516110, "pointer/blue.png", "CoCo都可(昌吉店)", "103台北市大同區昌吉街53號", "0225857669"],
+  [25.064633, 121.524460, "pointer/blue.png", "可不可熟成紅茶 台北晴光店", "10491台北市中山區雙城街15-5號", "0225851515"]
 ];
 
 function initMap(){
@@ -69,6 +70,7 @@ function initMap(){
       number=ip[5];
       content=`<div class="detail"><h2>${title}</h2>\n<h3>地址: </h3>${address}\n<h3>電話: </h3>${number}</div>`;
       infoWindow.setContent(content);
+      infoWindow.setZIndex(10);
       infoWindow.open(map, marker);
     });
     
